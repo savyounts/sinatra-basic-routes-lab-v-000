366d370f50b887029f4742cfd98d9d142e6c145f
Sav Younts <savyounts@gmail.com>
2018-09-03 02:34:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Savannah"
+  end
+
+  get '/hometown' do
+    "My hometown is Simpsonville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Drops of Jupiter"
+  end
 end
